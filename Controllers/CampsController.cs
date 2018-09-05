693588a335fa95723eb6ccc29232cd5d7e4dfadc
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-09-05 15:15:15 -0400

    Sep-5-2018_3:15_PM

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileBackend.Controllers
+{
+    public class CampsController : Controller
+    {
+        [HttpGet("api/camps")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Shawn of the Dead", FavoriteColor = "Plum" });
+        }
+    }
+}
